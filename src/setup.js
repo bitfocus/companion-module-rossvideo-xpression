69200edcde3993624af48345f431795691754660
Keith Rocheck <749812+krocheck@users.noreply.github.com>
2023-02-19 12:49:55 -0500

    Companion v3 upgrade

--- src/setup.js
+++ src/setup.js
@@ -1,5 +1,5 @@
-module.exports = {
-	FRAMEBUFFER_FIELD: {
+export const FIELDS = {
+	FRAMEBUFFER: {
 		type: 'number',
 		label: 'Framebuffer',
 		id: 'fb',
@@ -7,7 +7,7 @@ module.exports = {
 		min: 1,
 		max: 100,
 	},
-	GPI_FIELD: {
+	GPI: {
 		type: 'number',
 		label: 'GPI',
 		id: 'gpi',
@@ -15,7 +15,7 @@ module.exports = {
 		min: 0,
 		max: 9999,
 	},
-	LAYER_FIELD: {
+	LAYER: {
 		type: 'number',
 		label: 'Layer',
 		id: 'layer',
@@ -23,7 +23,7 @@ module.exports = {
 		min: -10,
 		max: 10,
 	},
-	TAKEID_FIELD: {
+	TAKEID: {
 		type: 'number',
 		label: 'Take ID',
 		id: 'takeID',
